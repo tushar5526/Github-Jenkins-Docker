d0dc30cc21ea843b27b75bdd03699eff8f2a6ce4
tushar5526 <30565750+tushar5526@users.noreply.github.com>
2020-05-13 16:31:35 +0530

    Added launch.py
    This will check the contents of the project and run respective docker container

---
+++ launch.py
@@ -0,0 +1,20 @@
+import os
+
+for file in os.listdir('/root/.jenkins/workspace/job1'):
+  ext = os.path.splitext(file0[-1].lower()
+
+  
+  if ext == '.php':
+    print('running httpd with php container')
+    os.system('docker run -it -v /root/.jenkins/workspace/job1/:/var/www/html -p 8081:8080 php')
+    break
+    
+  if ext == '.py':
+    print('running httpd with python3 container')
+    os.system('docker run -it -v /root/.jenkins/workspace/job1/:/var/www/html -p 8081:8080 python')
+    break
+    
+  if ext == '.html':
+    print('running httpd container')
+    os.system('docker run -it -v /root/.jenkins/workspace/job1/:/var/www/html -p 8081:8080 httpd')
+    break
